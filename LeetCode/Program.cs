a0d3dfae898c47770d4ef1413c0e50a530f3b572
Julius Bacosa <juliusbacosa@gmail.com>
2020-10-14 21:30:11 +0800

    leetcode init

---
+++ LeetCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i <= nums.Count(); i++)
+            {
+                for (int j = 1; j < nums.Count(); j++)
+                {
+                    if (target == (nums[i] + nums[j]))
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
