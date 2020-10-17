58aac4cc866e414ac16489b43e0ad68b04da2714
Julius Bacosa <juliusbacosa@gmail.com>
2020-10-17 14:56:25 +0800

    leetcode

--- LeetCode.Test/Tests.cs
+++ LeetCode.Test/Tests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace LeetCode.Test
@@ -18,10 +19,36 @@ namespace LeetCode.Test
         [InlineData(new int[] { 2, 7, 11, 15 }, new int[] { 0, 1 }, 9)]
         [InlineData(new int[] { 1,8 }, new int[] { 0, 1 }, 9)]
         [InlineData(new int[] { 3, 2, 7 }, new int[] { 1, 2 }, 9)]
-        public void Test1(int[] r, int[] o, int t)
+        public void TwoSumTest(int[] r, int[] o, int t)
         {
             var result = Program.TwoSum(r, t);
             Assert.Equal(result, o);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 9, 9, 9, 9, 9, 9, 9 }, 
+                    new int[] { 9, 9, 9, 9 }, 
+                    new int[] { 8, 9, 9, 9, 0, 0, 0, 1 },
+                    "89990001")]
+        [InlineData(new int[] { 2, 4, 3 },
+                    new int[] { 5, 6, 4 },
+                    new int[] { 7, 0, 8 },
+                    "708")]
+        [InlineData(new int[] { 2, 4, 9 },
+                    new int[] { 5, 6, 4, 9 },
+                    new int[] { 7, 0, 4, 0, 1},
+                    "70401")]
+        public void TestAddTwoNumbers(int[] p1, int[] p2, int[] expected, string expectedStr)
+        {
+            var l1 = Program.ArrayToLinkedList(p1);
+            var l2 = Program.ArrayToLinkedList(p2);
+            var result = Program.AddTwoNumbers(l1, l2);
+            List<int> tr = new List<int>();
+            (ListNode n, List<int> l) r = Program.traverse(result, tr);
+            var ROutput = r.l.ToArray();
+            var RExpected = expected;
+            Assert.Equal(ROutput, RExpected);
+            Assert.Equal(expectedStr, String.Join("", ROutput));
         }
     }
 }
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace LeetCode
 {
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
     public class Program
     {
+
         static void Main(string[] args)
         {
            
@@ -33,5 +46,54 @@ namespace LeetCode
         }
     
     
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            List<int> l1r = new List<int>(), l2r = new List<int>();
+            (ListNode n, List<int> l) r1 = traverse(l1, l1r);
+            (ListNode n, List<int> l) r2 = traverse(l2, l2r);
+            var r1d = ToDigit(r1.l);
+            var r2d = ToDigit(r2.l);
+            var total = r1d + r2d;
+            return ToLinkedList(total);
+        }
+
+        public static ListNode ToLinkedList(int digit)
+        {
+            var strDigit = digit.ToString();
+            var cr = strDigit.ToCharArray();
+            ListNode n = new ListNode(GetNumericValue(cr[0]));
+            for(var i = 1; i < cr.Length; i++)
+                n = new ListNode(GetNumericValue(cr[i]), n);
+            return n;
+        }
+
+        public static (ListNode, List<int>) traverse(ListNode n, List<int> lr)
+        {
+            lr.Add(n.val);
+            if (n.next != null)
+                traverse(n.next, lr);
+            return (n, lr);
+        }
+
+        public static int GetNumericValue(char c) =>
+            (int) Char.GetNumericValue(c);
+        public static int ToDigit(List<int> l) => 
+            Int32.Parse(String.Join("", l.ToArray()));
+
+
+        public static ListNode ArrayToLinkedList(int[] r)
+        {
+            ListNode ll = new ListNode(r[0]);
+            for (int i = 1; i < r.Length; i++)
+                ll = new ListNode(r[i], ll);
+            return ll;
+        }
+
     }
 }
